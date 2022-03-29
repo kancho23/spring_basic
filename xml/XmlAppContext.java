6a21aab8a69b2820a157e8a753df714cb8ffaf93
kancho23 <92067836+kancho23@users.noreply.github.com>
2022-03-30 02:13:06 +0900

    src_xml

---
+++ xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    /*이렇게만 두면 오류남, resources에 추가해줘야함*/
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
